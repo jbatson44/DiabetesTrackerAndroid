d1d4c88d575c9e7e2a28ebb71cc0e89ad1fb6d86
jbatson44 <bat14009@byui.edu>
2021-01-07 13:52:16 -0700

    Set up inputs for adding blood sugar values.

--- app/src/main/java/batson/diabetestrack/MainActivity.java
+++ app/src/main/java/batson/diabetestrack/MainActivity.java
@@ -3,9 +3,14 @@ package batson.diabetestrack;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -23,5 +28,44 @@ public class MainActivity extends AppCompatActivity {
         TextView date = findViewById(R.id.chosenDate);
         SimpleDateFormat dateFormat = new SimpleDateFormat("M/d/yy");
         date.setText(dateFormat.format(Calendar.getInstance().getTime()));
+
+        TextView time = findViewById(R.id.bloodSugarTime);
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        time.setText(timeFormat.format(Calendar.getInstance().getTime()));
+
+        time.setOnClickListener(v -> {
+            final Calendar cldr = Calendar.getInstance();
+            int hour = cldr.get(Calendar.HOUR_OF_DAY);
+            int minutes = cldr.get(Calendar.MINUTE);
+            // time picker dialog
+            TimePickerDialog picker = new TimePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                    (tp, sHour, sMinute) -> {
+                        String amPm = "AM";
+                        String hours = String.valueOf(sHour);
+                        String minutes1 = String.valueOf(sMinute);
+
+                        if (sHour > 12)
+                        {
+                            amPm = "PM";
+                            hours = String.valueOf(sHour - 12);
+                        }
+
+                        if (sMinute < 10)
+                        {
+                            minutes1 = "0" + sMinute;
+                        }
+
+                        time.setText(hours + ":" + minutes1 + " " + amPm);
+                    }, hour, minutes, false);
+
+            picker.show();
+        });
+
+        Button addBloodSugar = findViewById(R.id.addBloodSugar);
+        addBloodSugar.setOnClickListener(v -> {
+
+        });
     }
+
+
 }
